9bb23c37635b5d00be0315ce27bc7c0b36f82419
Jimmy Ji <Jimmy.Ji@bamboonetworks.com>
2018-12-26 16:02:56 +0800

    修改配置文件以及Dockerfile

--- course-dubbo-service/build.sh
+++ course-dubbo-service/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mvn clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true -Pprod
 
 cp ./src/main/resources/application-prod.properties ./target/application-prod.properties
 
