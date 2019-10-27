e4a90cc9851d43e746890051be2a1c32afbad0b1
JIZHE <jizhe@live.cn>
2019-10-27 11:32:16 +0800

    调整pom文件

--- user-thrift-service/src/main/java/com/xxx/user/UserServiceApplication.java
+++ user-thrift-service/src/main/java/com/xxx/user/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.xxx.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Description: 项目启动类
