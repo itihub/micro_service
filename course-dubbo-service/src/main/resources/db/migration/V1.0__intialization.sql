78b1e452ff597841446083d4e86be1ed52d973e1
JIZHE <jizhe@live.cn>
2019-11-07 21:58:59 +0800

    修改项目脚本

--- course-dubbo-service/src/main/resources/db/migration/V1.0__intialization.sql
+++ course-dubbo-service/src/main/resources/db/migration/V1.0__intialization.sql
@@ -1,12 +1,27 @@
 
--- 创建数据库
--- CREATE DATABASE db_course;
+# Create Database
+# ------------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS db_course DEFAULT CHARACTER SET = utf8mb4;
 
--- 用户表
+
+# Dump of table pe_course
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `pe_course`;
+
 CREATE TABLE `pe_course` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(32) NOT NULL COMMENT '用户名',
-  `description` varchar(255) NOT NULL COMMENT '用户密码',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `title` varchar(32) NOT NULL COMMENT '课程标题',
+  `description` varchar(255) NOT NULL COMMENT '描述',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='课程表';
+
+
+# Config
+# ------------------------------------------------------------
+INSERT INTO `pe_course` (`title`, `description`)
+VALUES
+	('语文', '中华文化'),
+	('数学', '探索数学的奥妙'),
+	('体育', '强身健体');
 
