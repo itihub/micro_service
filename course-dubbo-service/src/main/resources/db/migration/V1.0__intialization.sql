f31988a652474163224e48db051684ff6d5f1bfd
JIZHE <jizhe@live.cn>
2019-11-08 16:24:53 +0800

    修改初始化脚本

--- course-dubbo-service/src/main/resources/db/migration/V1.0__intialization.sql
+++ course-dubbo-service/src/main/resources/db/migration/V1.0__intialization.sql
@@ -16,6 +16,19 @@ CREATE TABLE `pe_course` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='课程表';
 
+# Dump of table pr_user_course
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `pr_user_course`;
+
+CREATE TABLE `pr_user_course` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `user_id` int(10) NOT NULL COMMENT '用户唯一标识',
+  `course_id` int(10) NOT NULL COMMENT '课程唯一标识',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IX_UNIQUE_KEY` (`user_id`,`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户课程关系表';
+
 
 # Config
 # ------------------------------------------------------------
@@ -25,3 +38,8 @@ VALUES
 	('数学', '探索数学的奥妙'),
 	('体育', '强身健体');
 
+INSERT INTO `pr_user_course` (`user_id`, `course_id`)
+VALUES
+	(2, 2),
+	(3, 1),
+	(2, 3);
