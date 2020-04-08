1bc5a44c5b90e315ecf63a8743d68a8f7c173717
JIZHE <jizhe@live.cn>
2020-04-08 14:55:36 +0800

    user-thrift-service 服务docker化

--- user-thrift-service/build.sh
+++ user-thrift-service/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 #重新构建docker镜像脚本
-mvn clean package -Dmaven.test.skip=true -Pprod
 
-cp ./src/main/resources/application-dev.properties ./target/application-dev.properties
+# 1.Maven打包
+mvn clean package -Dmaven.test.skip=true
 
+# 2.拷贝Spring配文件
+cp ./src/main/resources/application-prod.properties ./target/application-prod.properties
+
+#3. 构建Docker镜像
 docker build -t user-thrift-service:latest .
