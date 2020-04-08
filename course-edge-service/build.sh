0e4bb5a4dc96b6b2257a620dce2eb5c0607da3d0
JIZHE <jizhe@live.cn>
2020-04-08 16:34:10 +0800

    course-edge-service 服务docker化

--- course-edge-service/build.sh
+++ course-edge-service/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mvn clean package -Dmaven.test.skip=true -Pprod
+mvn clean package -Dmaven.test.skip=true
 
-cp ./src/main/resources/application-local.properties ./target/application-local.properties
+cp ./src/main/resources/application-prod.properties ./target/application-prod.properties
 
 docker build -t course-edge-service:latest .
