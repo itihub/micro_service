2206956031116afbd1f7f5e83f5e7f5b7e18028d
JIZHE <jizhe@live.cn>
2020-04-08 16:37:35 +0800

    api-gateway-zuul 服务docker化

--- api-gateway-zuul/build.sh
+++ api-gateway-zuul/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-mvn clean package -Dmaven.test.skip=true -Pprod
+# 1.Maven打包
+mvn clean package -Dmaven.test.skip=true
 
+# 2.拷贝Spring配文件
+cp ./src/main/resources/application-prod.properties ./target/application-prod.properties
 
+#3. 构建Docker镜像
 docker build -t api-gateway-zuul:latest .
