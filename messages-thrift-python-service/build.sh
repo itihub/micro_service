a6368c26ed6e5464f2ee7db808fcd179ded6d0ae
JIZHE <jizhe@live.cn>
2020-04-08 16:41:34 +0800

    message-thrift-python-service 服务docker化 (未验证)

--- messages-thrift-python-service/build.sh
+++ messages-thrift-python-service/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+
+# 1. 构建Docker镜像
 docker build -t message-service:latest .
+
 docker push message-service:latest
--- messages-thrift-python-service/build_base.sh
+++ messages-thrift-python-service/build_base.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# 构建带有thrift模块的Pyhton基础镜像
 docker build -t python-base:latest -f Dockerfile.base .
